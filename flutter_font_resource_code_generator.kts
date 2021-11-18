cd6d407bb1e29151eabffbb2f7f2021859d39c93
ShakirEmstell <shakir.m@emstell.com>
2021-11-18 11:49:11 +0530

    Update flutter_font_resource_code_generator.kts

--- flutter_font_resource_code_generator.kts
+++ flutter_font_resource_code_generator.kts
@@ -1,38 +1,40 @@
-
 /****
  *
  * ### font code for yaml file + font static reference in dart code ######
  * copy paste font path using IDE
  * select all font file -> right click -> copy path -> copy path from repo root -> paste below
+ * #### place font section inside flutter section in yaml file
  *
  *
  * */
 
 val s = """
-    fonts/museo_sans_100.ttf
-    fonts/museo_sans_300.ttf
-    fonts/museo_sans_500.ttf
-    fonts/museo_sans_500_italic.ttf
-    fonts/museo_sans_700.ttf
-    fonts/museo_sans_cyrl_100_ital.ttf
-    fonts/museo_sans_cyrl_300_ital.ttf
-    fonts/museo_sans_cyrl_700_ital.ttf
-    fonts/museo_sans_cyrl_900.ttf
-    fonts/museo_sans_cyrl_900_ital.ttf
-    fonts/tajawal_bold.ttf
-    fonts/tajawal_extra_bold.ttf
-    fonts/tajawal_extra_light.ttf
-    fonts/tajawal_light.ttf
-    fonts/tajawal_medium.ttf
-    fonts/tajawal_regular.ttf
-    fonts/tajawala_black.ttf
+fonts/museo_sans_100.ttf
+fonts/museo_sans_300.ttf
+fonts/museo_sans_500.ttf
+fonts/museo_sans_500_italic.ttf
+fonts/museo_sans_700.ttf
+fonts/museo_sans_cyrl_100_ital.ttf
+fonts/museo_sans_cyrl_300_ital.ttf
+fonts/museo_sans_cyrl_700_ital.ttf
+fonts/museo_sans_cyrl_900.ttf
+fonts/museo_sans_cyrl_900_ital.ttf
+fonts/tajawal_bold.ttf
+fonts/tajawal_extra_bold.ttf
+fonts/tajawal_extra_light.ttf
+fonts/tajawal_light.ttf
+fonts/tajawal_medium.ttf
+fonts/tajawal_regular.ttf
+fonts/tajawala_black.ttf
 """.trimIndent()
 
 
 
 
 fun namify(string: String): String {
-    return string.split(".")[0].replace(" ", "_")
+    return string.split(".")[0]
+        .replace(" ", "_")
+        .replace("-", "_")
         .replace("_____", "_")
         .replace("____", "_")
         .replace("___", "_")
@@ -41,9 +43,9 @@ fun namify(string: String): String {
 
 
 fun getFontScriptForYamlFile(fileName: String, displayname: String): String {
-  return "    - family: $displayname\n"+
-           "      fonts:\n"+
-           "        - asset: $fileName\n\n"
+    return "    - family: $displayname\n"+
+            "      fonts:\n"+
+            "        - asset: $fileName\n\n"
 
 }
 
@@ -70,9 +72,10 @@ println("class MyFont {\n" +
             val displayname = namify(fileName.split("/").last())
             "  static const String $displayname = '$displayname';\n"
         }.joinToString("")
-         +
+        +
         "\n" +
         "}\n")
+
 
 
 
